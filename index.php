b84caa5d9db924c8577d0508a7fec403ec3e375b
daniel_smnjuntak <daniel.smnjuntak@gmail.com>
2020-01-15 01:00:56 +0700

    update db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "elcomp.database.windows.net";
+    $user = "daniel.smnjuntak";
+    $pass = "ElderWInte96";
+    $db = "elcomp";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
